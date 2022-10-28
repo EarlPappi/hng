dfad5ad00ac3d55e80c99bb3c14bd5b07f8f6540
earlpappi <earlpappi@gmail.com>
2022-10-28 17:32:06 +0100

    --commits: little padding fixes

--- src/Compoments/Link.jsx
+++ src/Compoments/Link.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Link({ link }) {
   return (
-    <a href={ link.url } id={ link.id } title={ link.subtext } className='block bg-[#EAECF0] font-medium my-4 p-3 text-center w-11/12 mx-auto rounded-md hover:bg-slate-500'>
+    <a href={ link.url } id={ link.id } title={ link.subtext } className='block bg-[#EAECF0] font-medium my-4 py-5 text-center w-11/12 mx-auto rounded-md hover:bg-slate-500'>
                 { link.linkName }
         </a>
   )
