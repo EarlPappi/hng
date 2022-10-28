9c8ac4504443873e5fb90c8be9117c43880a3d6d
earlpappi <earlpappi@gmail.com>
2022-10-28 16:07:05 +0100

    --commits: fixes

--- src/Compoments/ProfileHeader.jsx
+++ src/Compoments/ProfileHeader.jsx
@@ -3,9 +3,9 @@ import Logo from "../Assets/profileAvatar.jpg";
 
 function ProfileHeader() {
   return (
-    <div className='bg-slate-200 p-3'>
+    <div className=' p-3'>
         <div className=''>
-            <img src={ Logo }  alt="" className='rounded-full w-[30%] md:w-[20%] mx-auto' id='profile__img'/>
+            <img src={ Logo }  alt="" className='rounded-full w-[20%] md:w-[15%] mx-auto' id='profile__img'/>
         </div>
 
         <h1 className='text-center font-bold text-2xl my-3' id='twitter'>earl_pappi</h1>
