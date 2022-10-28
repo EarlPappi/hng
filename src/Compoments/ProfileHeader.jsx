f5bb92bf678d5d2266c4348313c81aa6e7ee86c1
earlpappi <earlpappi@gmail.com>
2022-10-28 17:29:25 +0100

    --commits: share icon fixes

--- src/Compoments/ProfileHeader.jsx
+++ src/Compoments/ProfileHeader.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import Logo from "../Assets/profileAvatar.jpg";
+import Share from '../Assets/shareIcon.svg'
 
 function ProfileHeader() {
-  return (
-    <div className=' p-3'>
-        <div className=''>
-            <img src={ Logo }  alt="" className='rounded-full w-[20%] md:w-[15%] mx-auto' id='profile__img'/>
-        </div>
+    return (
+        <div className=' p-3'>
+            <div className='relative'>
+                <img src={Logo} alt="" className='rounded-full w-[20%] md:w-[15%] mx-auto' id='profile__img' />
 
-        <h1 className='text-center font-bold text-2xl my-3' id='twitter'>earl_pappi</h1>
-        <h1 className='hidden text-center font-bold text-2xl my-3' id='slack'>earl</h1>
-    </div>
-  )
+                <div className='absolute top-2 right-5 md:right-10 md:top-20 hover:bg-slate-300 p-3 md:p-5 rounded-full border-2 border-dashed hover:border-none'>
+                    <img src={Share} alt="" />
+                </div>
+
+            </div>
+
+            <h1 className='text-center font-bold text-2xl my-3' id=''>earl_pappi</h1>
+            <h1 className='hidden text-center font-bold text-2xl my-3' id='slack'>earl</h1>
+        </div>
+    )
 }
 
 export default ProfileHeader
