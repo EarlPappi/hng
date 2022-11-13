a471a62ef85452c31e351bc0b80440245b7abc12
earlpappi <earlpappi@gmail.com>
2022-11-13 01:09:46 +0100

    --commits: footer mobile view done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,61 +7,63 @@ import instagram from '../../Asset/footerIG.png'
 import twitter from '../../Asset/footerTwitter.png'
 
 function Footer() {
-  return (
-    <div className='bg-[#1D1D1E] py-12'>
-        <Wrapper>
-            <footer className='flex justify-between'>
-                <div className='flex justify-between gap-4 flex-col h-max'>
-                    <div className='p-12 pl-0 pt-0'>
-                        <img src={ footerLogo } alt="" />
+    return (
+        <div className='bg-[#1D1D1E] py-12'>
+            <Wrapper>
+                <footer className='flex flex-col-reverse md:flex-row justify-between'>
+                    <div className='flex justify-between gap-4 flex-col h-max w-full md:w-2/5'>
+                        <div className='p-12 pl-0 pt-0'>
+                            <img src={footerLogo} alt="" />
+                        </div>
+
+                        <div className='flex gap-4 p-12 pl-0'>
+                            <img src={facebook} alt="" />
+                            <img src={instagram} alt="" />
+                            <img src={twitter} alt="" />
+                        </div>
+
+                        <p className='text-white'>&copy; 2022 Metabnb</p>
+
                     </div>
 
-                    <div className='flex gap-4 p-12 pl-0'>
-                        <img src={facebook} alt="" />
-                        <img src={instagram} alt="" />
-                        <img src={twitter} alt="" />
+                    <div className='flex flex-wrap w-full md:w-3/5 justify-between my-8'>
+                        <div>
+                            <p className='font-bold text-[1.2em] text-white'>Commmunity</p>
+
+                            <div className='flex flex-col gap-4 mt-4'>
+                                <Link to='/' className='text-white font-light'>NFT</Link>
+                                <Link to='/' className='text-white font-light'>Token</Link>
+                                <Link to='/' className='text-white font-light'>Landlords</Link>
+                                <Link to='/' className='text-white font-light'>Discord</Link>
+                            </div>
+                        </div>
+
+                        <div>
+                            <p className='font-bold text-[1.2em] text-white'>Places</p>
+
+                            <div className='flex flex-col gap-4 mt-4'>
+                                <Link to='/' className='text-white font-light'>Castle</Link>
+                                <Link to='/' className='text-white font-light'>Farm</Link>
+                                <Link to='/' className='text-white font-light'>Beach</Link>
+                                <Link to='/' className='text-white font-light'>Learn More</Link>
+                            </div>
+                        </div>
+
+                        <div>
+                            <p className='font-bold text-[1.2em] text-white'>About Us</p>
+
+                            <div className='flex flex-col gap-4 mt-4'>
+                                <Link to='/' className='text-white font-light'>Roadmap</Link>
+                                <Link to='/' className='text-white font-light'>Creator</Link>
+                                <Link to='/' className='text-white font-light'>Career</Link>
+                                <Link to='/' className='text-white font-light'>Contact Us</Link>
+                            </div>
+                        </div>
                     </div>
-
-                    <p className='text-white'>&copy; 2022 Metabnb</p>
-
-                </div>
-
-                <div>
-                    <p className='font-bold text-[1.2em] text-white'>Commmunity</p>
-
-                    <div className='flex flex-col gap-4 mt-4'>
-                        <Link to='/' className='text-white font-light'>NFT</Link>
-                        <Link to='/' className='text-white font-light'>Token</Link>
-                        <Link to='/' className='text-white font-light'>Landlords</Link>
-                        <Link to='/' className='text-white font-light'>Discord</Link>
-                    </div>
-                </div>
-
-                <div>
-                    <p className='font-bold text-[1.2em] text-white'>Places</p>
-
-                    <div className='flex flex-col gap-4 mt-4'>
-                        <Link to='/' className='text-white font-light'>Castle</Link>
-                        <Link to='/' className='text-white font-light'>Farm</Link>
-                        <Link to='/' className='text-white font-light'>Beach</Link>
-                        <Link to='/' className='text-white font-light'>Learn More</Link>
-                    </div>
-                </div>
-
-                <div>
-                    <p className='font-bold text-[1.2em] text-white'>About Us</p>
-
-                    <div className='flex flex-col gap-4 mt-4'>
-                        <Link to='/' className='text-white font-light'>Roadmap</Link>
-                        <Link to='/' className='text-white font-light'>Creator</Link>
-                        <Link to='/' className='text-white font-light'>Career</Link>
-                        <Link to='/' className='text-white font-light'>Contact Us</Link>
-                    </div>
-                </div>
-            </footer>
-        </Wrapper>
-    </div>
-  )
+                </footer>
+            </Wrapper>
+        </div>
+    )
 }
 
 export default Footer
--- src/Components/Sections/PreFooter.jsx
+++ src/Components/Sections/PreFooter.jsx
@@ -7,17 +7,17 @@ function PreFooter() {
     return (
         <div className='bg-[#A02279] py-12'>
             <Wrapper>
-                <div className='flex gap-12 items-center justify-between'>
-                    <div className='w-1/2'>
-                        <h1 className='w-8/12 text-[2.5em] text-white leading-[120%] mb-4'>Metabnb NFTs</h1>
+                <div className='flex flex-col-reverse md:flex-row gap-12 items-center justify-between'>
+                    <div className='w-full md:w-1/2'>
+                        <h1 className='md:w-8/12 text-[2.5em] text-white leading-[120%] mb-4 text-center md:text-left'>Metabnb NFTs</h1>
 
-                        <p className='w-8/12 text-white leading-[200%] my-8 font-thin'>Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.</p>
+                        <p className='md:w-8/12 text-white leading-[200%] my-8 font-thin'>Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.</p>
 
                         <Link to='/' className='bg-white text-[#A02279] px-4 py-2 rounded-sm'>Learn More</Link>
 
                     </div>
 
-                    <div className='w-1/2'>
+                    <div className='w-full md:w-1/2'>
                         <img src={preImg} alt="" className='w-full' />
 
                     </div>
