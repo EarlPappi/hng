743bbfaab3466beda553a32343bc4c2609da05ee
earlpappi <earlpappi@gmail.com>
2022-11-19 10:01:24 +0100

    feat: forget password modal bg

--- src/Pages/Profile/PasswordModal.jsx
+++ src/Pages/Profile/PasswordModal.jsx
@@ -17,7 +17,7 @@ function PasswordModal({ showPassModal, setShowPassModal }) {
                     <div className='absolute bg-white w-11/12 md:w-8/12 h-5/6 z-20 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] p-3 md:p-6 rounded-3xl'>
                         <div className='w-11/12 mx-auto overflow-hidden'>
                             {/* Close Button */}
-                            <p className='inline rounded-full py-2 px-4  absolute top-[-2%] right-[-1%] z-40 font-bold text-xl bg-[#648B7A] text-white cursor-pointer border-4 border-white'
+                            <p className='inline rounded-full py-2 px-4  absolute top-[-2%] right-[-1%] z-40 font-bold text-xl bg-primaryMain text-white cursor-pointer border-4 border-white'
                                 onClick={() => {
                                     setShowPassModal(false)
                                 }}>X</p>
