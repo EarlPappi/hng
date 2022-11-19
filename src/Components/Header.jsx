eb287bcf98e76f803b6ba16fd3aaac0752f66610
earlpappi <earlpappi@gmail.com>
2022-11-19 08:58:16 +0100

    feat: responsive header with menu option

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import { MainIcon } from '../Icons'
+import React, { useState } from 'react'
+import { MainIcon, HamburgerIcon, UserProfile } from '../Icons'
 import Wrapper from './Wrapper/Wrapper'
 
 function Header() {
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
         <div className='bg-white my-4'>
             <Wrapper>
@@ -10,14 +12,42 @@ function Header() {
                 <div className='flex justify-between items-center'>
                     <div>
                         <MainIcon />
+                    </div>
+
+                    <div className='flex gap-4 items-center'>
+                        <p className='hidden md:block text-[#0652DD] font-semibold border-2 border-[#0652DD] py-2 px-4 rounded-lg'>History</p>
+
+                        <p className='rounded-full px-5 py-3 text-[#0652DD] font-semibold bg-[#CDDCF8]' onMouseOver={() => {
+                            setShowMenu(true)
+                        }} >B</p>
+
+                        <div className="md:hidden" onClick={() => {
+                            setShowMenu(!showMenu)
+                        }}>
+                            <HamburgerIcon />
+                        </div>
 
                     </div>
 
-                    <div className='flex gap-4'>
-                        <p className='text-[#0652DD] font-semibold border-2 border-[#0652DD] py-2 px-4 rounded-lg'>History</p>
+                    {showMenu &&
+                        <div onMouseOver={() => {
+                            setShowMenu(true)
+                        }} onMouseLeave={() => {
+                            setShowMenu(false)
+                        }} className='absolute w-max h-max top-[4em] right-[0.5em] bg-white border rounded-md border-[#CAD0DD]'>
+                            <p onClick={() => {
+                                setShowMenu(false)
+                            }}>Close</p>
 
-                        <p className='rounded-full px-5 py-3 text-[#0652DD] font-semibold bg-[#CDDCF8]'>B</p>
-                    </div>
+                            <p className='px-[4em] py-[2em] flex items-center gap-4 justify-center  border-b border-b-[#CAD0DD]'>
+                                <span><UserProfile /></span>
+                                <span className='font-bold'>Account</span>
+                            </p>
+
+                            <p className='px-[4em] py-[2em] text-center font-bold'>Logout</p>
+
+                        </div>
+                    }
                 </div>
             </Wrapper>
         </div>
