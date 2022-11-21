eebb41552347b78a6d198d113339179f0405a933
Earl Pappi <94018014+EarlPappi@users.noreply.github.com>
2022-11-21 12:49:44 +0100

    Revert "Stage 4 garet"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import Navbar, { Footer } from "./Components/Navbar/Navbar";
-
-import "./App.css";
-import Sidebar from "./Components/Navbar/Sidebar";
-import Hero from "./Components/Sections/Hero/Hero";
-import { UserProvider } from "./Store/Context";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <UserProvider>
-      <div className="w-full h-screen">
-        <div className="navbar">
-          <Navbar />
-        </div>
-        <div className="flex">
-          <Sidebar />
-          <Hero />
-        </div>
-        <Footer />
-      </div>
-    </UserProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
